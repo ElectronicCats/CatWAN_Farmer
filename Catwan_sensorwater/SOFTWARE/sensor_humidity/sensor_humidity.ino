f86a6d9584a8889686bf69d7d7f996817ca6501a
ROCIO RODRIGUEZ <rocio95cua@gmail.com>
2018-10-03 19:22:09 -0500

    Read voltaje divisor SAMD21

--- Catwan_sensorwater/SOFTWARE/sensor_humidity/sensor_humidity.ino
+++ Catwan_sensorwater/SOFTWARE/sensor_humidity/sensor_humidity.ino
@@ -1,44 +1,40 @@
-int digPin=10;//output PWM
-int pinOUT =0;
-int pinIN =0;
+int digPin=3;//output PWM
+float pinOUT =0;
+float pinIN =0;
 float Vin =0;
 float Vout =0;
-int R=0;
+float R=0;
 
 void setup()
 {
-  pinMode(digPin, OUTPUT);//Declaration output digital pin 10
-  Serial.begin(9600);
+  pinMode(3, OUTPUT);
+  analogWriteResolution(10);
+  analogReadResolution(12);
+  SerialUSB.begin(9600);
 }
 
 void loop() 
 {
-  analogWrite(digPin,200); //63 25%/ 127 50%/ 190 75%/255 100% duty cicle señal 490hz I need 50ms and take sample 100us
+  digitalWrite(digPin,512); //63 25%/ 127 50%/ 190 75%/255 100% duty cicle señal 490hz I need 50ms and take sample 100us
 
-  /*digitalWrite(digPin,HIGH);
-  delay(50);
-  digitalWrite(digPin,LOW);
-  delay(50);*/
-  
-  pinOUT=analogRead(A0);
- // delay(.1);
-  Vout=(5*pinOUT)/1023;
-  pinIN=analogRead(A1);
- // delay(.1);
-  Vin=(5*pinIN)/1023;
+  pinOUT=analogRead(A1);
+  Vout=(3.3*pinOUT)/4096;
+  pinIN=analogRead(A2);
+  Vin=(3.3*pinIN)/4096;
   R=7800*(Vout-Vin)/Vin;
  
-  Serial.print("Lectura A0: ");
-  Serial.print(pinOUT);
-  Serial.print(" ");
-  Serial.print(Vout);
-  Serial.println(" ");
-  Serial.print("Lectura A1: ");
-  Serial.print(pinIN);
-  Serial.print(" ");
-  Serial.print(Vin);
-  Serial.println(" ");
-  Serial.print("Resistencia: ");
-  Serial.print(R);
-  Serial.println(" ");
+  SerialUSB.print("Lectura A0: ");
+  SerialUSB.print(pinOUT);
+  SerialUSB.print(" ");
+  SerialUSB.print(Vout);
+  SerialUSB.println(" ");
+  SerialUSB.print("Lectura A1: ");
+  SerialUSB.print(pinIN);
+  SerialUSB.print(" ");
+  SerialUSB.print(Vin);
+  SerialUSB.println(" ");
+  SerialUSB.print("Resistencia: ");
+  SerialUSB.print(R);
+  SerialUSB.println(" ");
+  delay(1000);
 }
