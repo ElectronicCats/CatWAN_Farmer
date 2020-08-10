fd722927b2f93af0f7ab9089589d4dbd2bbd779d
Montse <figueroa.marimontse@gmail.com>
2020-08-10 17:28:00 -0500

    WM_Resistance and sleep 10s

--- Firmware/CatWAN_Farmer/CatWAN_Farmer.ino
+++ Firmware/CatWAN_Farmer/CatWAN_Farmer.ino
@@ -1,4 +1,4 @@
-/*******************************************************
+ /*******************************************************
   Firmware for CatWAN Farmer
   Andres Sabas @ Electronic Cats
   Montserrat Figueroa @ Electronic Cats
@@ -15,18 +15,17 @@
 
 #include "DHT.h"
 #include <lorawan.h>
-#include <RTCZero.h>
 
-
-/* Create an rtc object */
-RTCZero rtc;
+#include <ArduinoLowPower.h>
+int flag=true;
 
 //#define SENSOR1
 //#define SENSOR2
 #define SENSOR3
 //#define SENSOR4
 
-//#define DEBUG
+#define DEBUG
+//#define DEBUG2
 
 
 typedef struct {        // Structure to be used in percentage and resistance values matrix to be filtered (have to be in pairs)
@@ -47,7 +46,13 @@ typedef struct {        // Structure to be used in percentage and resistance val
 #define DHTTYPE DHT22
 
 DHT dht(DHTPIN, DHTTYPE);
+float tempC=24.0;
 
+long WM1_Resistance,WM2_Resistance, WM3_Resistance,WM4_Resistance=0;
+const long short_resistance=-20;
+const long open_resistance=-100;
+const long short_CB=240;
+const long open_CB=255;
 const long knownResistor = 4700;  // Value of R1 and R2 in Ohms, = reference for sensor
 
 int supplyVoltage;      // Measured supply voltage
@@ -57,31 +62,20 @@ int zeroCalibration = 95;        // calibrate sensor resistace to zero when inpu
 
 values valueOf[NUM_READS];        // Calculated  resistances to be averaged
 long buffer[NUM_READS];
-int indice;
+int indice=0;
 int i;                            // Simple index variable
 int j = 0;                        // Simple index variable
-
-
-/* Change these values to set the current initial time */
-const byte seconds = 0;
-const byte minutes = 00;
-const byte hours = 17;
-
-/* Change these values to set the current initial date */
-const byte day = 17;
-const byte month = 11;
-const byte year = 15;
 
 void setup() 
 {
 
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);             // initialize LoRa module communications
-  while (!SerialUSB) ; // Wait for Serial monitor to open
+  while (!Serial) ; // Wait for Serial monitor to open
 
   // setting up the sensor interface
   // initialize digital pins D5, D6 as an high impedance input.
-  // Pin 5,6 are for driving the soil moisture sensor
+  // Pin 5,6 are f846r driving the soil moisture sensor
   pinMode(SENS_X, INPUT); // SENS_X
   pinMode(SENS_Y, INPUT); // SENS_Y
   // Pin 7 is for enabling Mux switches
@@ -93,83 +87,63 @@ void setup()
   Serial.println(F("DHT Init!"));
   dht.begin();
 
-
- pinMode(LED_BUILTIN, OUTPUT);
- digitalWrite(LED_BUILTIN, LOW);
-
-  rtc.begin();
-
-  rtc.setTime(hours, minutes, seconds);
-  rtc.setDate(day, month, year);
-
-  rtc.setAlarmTime(17, 00, 10);
-  rtc.enableAlarm(rtc.MATCH_HHMMSS);
-
-  rtc.attachInterrupt(alarmMatch);
-
-  rtc.standbyMode();
- 
+  pinMode(LED_BUILTIN, OUTPUT);
+  #ifndef DEBUG
+  // Uncomment this function if you wish to attach function dummy when RTC wakes up the chip
+   LowPower.attachInterruptWakeup(RTC_ALARM_WAKEUP, lectura, CHANGE);
+   #endif
 
 }
-
 
 void loop()
 {
-  soilsensors();
-
-  delay (100);
-
-
-  temperatura();
-  delay(100);
-
-  rtc.standbyMode();    // Sleep until next alarm match
-  delay(100);
+  if(flag==true){
+   temperatura();
+   soilsensors();
+//float   cb1 = converter_cb(WM1_Resistance);
+//float   cb2 = converter_cb(WM2_Resistance);
+float   cb3 = converter_cb(WM3_Resistance);
+//float   cb4 = converter_cb(WM4_Resistance);
+  Serial.print("CB3:");
+  Serial.println(cb3);
+   //envio();
+  }
+  // Triggers a 2000 ms sleep (the device will be woken up only by the registered wakeup sources and by internal RTC)
+  // The power consumption of the chip will drop consistently
+  #ifndef DEBUG
+  LowPower.sleep(10000);
+  #endif
+  
 }
 
-
-void alarmMatch()
+void lectura() 
 {
-  digitalWrite(LED_BUILTIN, HIGH);
+ flag = true;
 }
-
-
 
 void temperatura()
 {
-  // Wait a few seconds between measurements.
-  delay(2000);
 
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
+  tempC = dht.readTemperature();
 
   // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
+  if (isnan(h) || isnan(tempC)) {
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
 
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
   // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
+  float hic = dht.computeHeatIndex(tempC, h, false);
 
   Serial.print(F("Humidity: "));
   Serial.print(h);
   Serial.print(F("%  Temperature: "));
-  Serial.print(t);
-  Serial.print(F("°C "));
-  Serial.print(f);
-  Serial.print(F("°F  Heat index: "));
-  Serial.print(hic);
-  Serial.print(F("°C "));
-  Serial.print(hif);
-  Serial.println(F("°F"));
+  Serial.print(tempC);
+  Serial.println(F("°C "));
 }
 
 void soilsensors() {
@@ -180,7 +154,9 @@ void soilsensors() {
   digitalWrite(S1, LOW);
   digitalWrite(ENABLE, LOW);
   measureSensor();
-  unsigned long read1 = average();
+  Serial.print("average: ");
+  Serial.println(average());
+  WM1_Resistance = average();
 #endif
 
 #ifdef SENSOR2
@@ -189,7 +165,7 @@ void soilsensors() {
   digitalWrite(S1, HIGH);
   digitalWrite(ENABLE, LOW);
   measureSensor();
-  unsigned long read2 = average();
+  WM2_Resistance = average();
 #endif
 
 #ifdef SENSOR3
@@ -197,8 +173,9 @@ void soilsensors() {
   digitalWrite(S0, HIGH);
   digitalWrite(S1, LOW);
   digitalWrite(ENABLE, LOW);
+  delay(10);
   measureSensor();
-  unsigned long read3 = average();
+  WM3_Resistance = average();
 #endif
 
 #ifdef SENSOR4
@@ -207,32 +184,31 @@ void soilsensors() {
   digitalWrite(S1, HIGH);
   digitalWrite(ENABLE, LOW);
   measureSensor();
-  unsigned long read4 = average();
+  WM4_Resistance = average();
 #endif
 
 
   float Vsys = analogRead(ADC_BAT) * 0.00647; // read the battery voltage
   delay (50);
 
-#ifdef SENSOR1
+
   //Print/send results
   Serial.print("Farmer");
   Serial.print(",");
-#endif
 #ifdef SENSOR1
-  Serial.println(read1);
+  Serial.println( WM1_Resistance);
   Serial.print(",");
 #endif
 #ifdef SENSOR2
-  Serial.print(read2);
+  Serial.print(WM2_Resistance);
   Serial.print(",");
 #endif
 #ifdef SENSOR3
-  Serial.println(read3);
+  Serial.println(WM3_Resistance);
   Serial.print(",");
 #endif
 #ifdef SENSOR4
-  Serial.print(read4);
+  Serial.print(WM4_Resistance);
   Serial.print(",");
 #endif
   Serial.println(Vsys, 2);
@@ -250,8 +226,8 @@ void measureSensor()
     pinMode(SENS_X, OUTPUT);
     digitalWrite(SENS_X, HIGH);
     delayMicroseconds(25);
-    sensorVoltage = analogRead(A1);   // read the sensor voltage
-    supplyVoltage = analogRead(A0);   // read the supply voltage
+    sensorVoltage = analogRead(A0);   // read the sensor voltage
+    supplyVoltage = analogRead(A1);   // read the supply voltage
     delayMicroseconds(25);
     digitalWrite(SENS_X, LOW);
     pinMode(SENS_X, INPUT);
@@ -259,7 +235,7 @@ void measureSensor()
     long resistance = (knownResistor * (supplyVoltage - sensorVoltage ) / sensorVoltage) - zeroCalibration ;
     addReading(resistance);
 
-#ifdef DEBUG
+#ifdef DEBUG2
     Serial.print("Resistor con: ");
     Serial.println(knownResistor);
     Serial.print("Supply V: ");
@@ -275,13 +251,17 @@ void measureSensor()
     digitalWrite(SENS_Y, LOW);
     digitalWrite(SENS_Y, HIGH);
     delayMicroseconds(25);
-    sensorVoltage = analogRead(A0);   // read the sensor voltage
-    supplyVoltage = analogRead(A1);   // read the supply voltage
+    sensorVoltage = analogRead(A1);   // read the sensor voltage
+    supplyVoltage = analogRead(A0);   // read the supply voltage
     delayMicroseconds(25);
     digitalWrite(SENS_Y, LOW);
     pinMode(SENS_Y, INPUT);
 
-#ifdef DEBUG
+    resistance = (knownResistor * (supplyVoltage - sensorVoltage ) / sensorVoltage) - zeroCalibration ;
+    delay(100);
+    addReading(resistance);
+
+#ifdef DEBUG2
     Serial.print("Resistor con: ");
     Serial.println(knownResistor);
     Serial.print("Supply V2: ");
@@ -292,9 +272,6 @@ void measureSensor()
     Serial.println(resistance);
 #endif
 
-    resistance = (knownResistor * (supplyVoltage - sensorVoltage ) / sensorVoltage) - zeroCalibration ;
-    delay(100);
-    addReading(resistance);
 
   }
 }
@@ -311,5 +288,62 @@ long average() {
   for (int i = 0; i < NUM_READS; i++) {
     sum += buffer[i];
   }
-  return (long)(sum / NUM_READS);
+  long promedio = (sum / NUM_READS);
+  return promedio;
+}
+
+int converter_cb(float WM_Resistance )
+{
+   float WM_CB=0;
+   Serial.println("WM_Resistance: ");
+   Serial.println(WM_Resistance);
+  //convert WM1 Reading to Centibars or KiloPascal
+    // The trailing 00 for numbers are very critical in the formula below eg: 1 is written as 1.00, else code messes math due to int vs double for numbers
+    if (WM_Resistance>550.00) 
+    {
+      if(WM_Resistance>8000.00)
+      {
+      WM_CB=-2.246-5.239*(WM_Resistance/1000.00)*(1+.018*(tempC-24.00))-.06756*(WM_Resistance/1000.00)*(WM_Resistance/1000.00)*((1.00+0.018*(tempC-24.00))*(1.00+0.018*(tempC-24.00))); 
+      Serial.print("Entered WM1 >8000 Loop \n");
+      } 
+     if (WM_Resistance>1000.00) 
+      {
+        if (WM_Resistance<8000)
+        {
+     WM_CB=(-3.213*(WM_Resistance/1000.00)-4.093)/(1-0.009733*(WM_Resistance/1000.00)-0.01205*(tempC)) ;
+     Serial.print("Entered WM1 >1000 Loop \n");
+        }
+      }
+      if (WM_Resistance<1000.00)
+        {
+        if (WM_Resistance>550.00)
+        {
+        WM_CB=-20.00*((WM_Resistance/1000.00)*(1.00+0.018*(tempC-24.00))-0.55);
+        Serial.print("Entered WM1>550 Loop \n");
+        }
+        }
+     }
+    if(WM_Resistance<550.00)
+      {
+        if(WM_Resistance>300.00)
+        {
+        WM_CB=0.00;
+        Serial.print("Entered 550<WM1>0 Loop \n");
+        }
+        if(WM_Resistance<300.00)
+        {
+          if(WM_Resistance>=short_resistance)
+          {   
+          WM_CB=short_CB; //240 is a fault code for sensor terminal short
+          Serial.print("Entered Sensor Short Loop WM1 \n");
+          }
+         
+        }
+        if(WM_Resistance<=open_resistance)
+        {
+        WM_CB=open_CB; //255 is a fault code for open circuit or sensor not present 
+        Serial.print("Entered Open or Fault Loop for WM1 \n");
+        }
+      }
+  return WM_CB;
 }
